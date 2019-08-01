7829bb536bd167142908ac04162f04a2fd5d9526
Shikii_ <33571437+Thegamesbg@users.noreply.github.com>
2019-08-01 17:56:47 +0300

    actually made the code working

--- bot.py
+++ bot.py
@@ -1,12 +1,25 @@
+# Use carefully!
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+	print("Ready to go! :3")
+
 @bot.command(pass_context=True)
 async def deleteall(ctx, channel):
 	for channel in ctx.message.author.server.channels:
-		delete_channel(channel)
+		await bot.delete_channel(channel)
 		
 		
 @bot.command(pass_context=True)
 async def banall(ctx, member):
-    for member in ctx.message.author.server.members:
-        ban(member)
+	for member in ctx.message.author.server.members:
+		await bot.ban(member)
 		
 bot.run("YOUR TOKEN GOES HERE")
